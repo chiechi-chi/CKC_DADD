61c05f13b6caa52ff283917d3a6b6bf680dfbc1b
anhhoai <lephonganhhoai9a1@gmail.com>
2022-12-01 21:01:38 +0700

    friendupdate

---
+++ doan_ltdd_chk/lib/DSFriends.dart
@@ -0,0 +1,258 @@
+import 'package:flutter/material.dart';
+
+class DSBanbe extends StatefulWidget {
+  const DSBanbe({super.key});
+
+  @override
+  State<DSBanbe> createState() => _DSBanbe();
+}
+
+class _DSBanbe extends State<DSBanbe> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        color: Color(0xff152B42),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 30, 0, 0),
+              child: const Text(
+                'Danh Sách ban be',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(10, 20, 0, 0),
+              child: Column(
+                children: [
+                  Container(
+                    width: 350,
+                    height: 650,
+                    color: Color(0xff152B39),
+                    child: Column(children: [
+                      Container(
+                        padding: EdgeInsets.fromLTRB(30, 10, 0, 0),
+                        child: ClipRRect(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Container(
+                                color: Color.fromARGB(255, 12, 1, 73),
+                                width: 300,
+                                height: 70,
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      'Ban bè 1',
+                                      style: TextStyle(
+                                        color:
+                                            Color.fromARGB(255, 255, 255, 255),
+                                        fontWeight: FontWeight.normal,
+                                      ),
+                                    ),
+                                    Container(
+                                      color: Color.fromARGB(255, 12, 1, 73),
+                                      width: 100,
+                                      height: 60,
+                                      child: Center(
+                                          child: Image(
+                                              image: AssetImage(
+                                                  'assets/223.png'))),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.fromLTRB(30, 20, 0, 0),
+                        child: ClipRRect(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Container(
+                                color: Color.fromARGB(255, 12, 1, 73),
+                                width: 300,
+                                height: 70,
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      'Ban bè 1',
+                                      style: TextStyle(
+                                        color:
+                                            Color.fromARGB(255, 255, 255, 255),
+                                        fontWeight: FontWeight.normal,
+                                      ),
+                                    ),
+                                    Container(
+                                      color: Color.fromARGB(255, 12, 1, 73),
+                                      width: 100,
+                                      height: 60,
+                                      child: Center(
+                                          child: Image(
+                                              image: AssetImage(
+                                                  'assets/223.png'))),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.fromLTRB(30, 20, 0, 0),
+                        child: ClipRRect(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Container(
+                                color: Color.fromARGB(255, 12, 1, 73),
+                                width: 300,
+                                height: 70,
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      'Ban bè 1',
+                                      style: TextStyle(
+                                        color:
+                                            Color.fromARGB(255, 255, 255, 255),
+                                        fontWeight: FontWeight.normal,
+                                      ),
+                                    ),
+                                    Container(
+                                      color: Color.fromARGB(255, 12, 1, 73),
+                                      width: 100,
+                                      height: 60,
+                                      child: Center(
+                                          child: Image(
+                                              image: AssetImage(
+                                                  'assets/2234.png'))),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.fromLTRB(30, 20, 0, 0),
+                        child: ClipRRect(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Container(
+                                color: Color.fromARGB(255, 12, 1, 73),
+                                width: 300,
+                                height: 70,
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      'Ban bè 1',
+                                      style: TextStyle(
+                                        color:
+                                            Color.fromARGB(255, 255, 255, 255),
+                                        fontWeight: FontWeight.normal,
+                                      ),
+                                    ),
+                                    Container(
+                                      color: Color.fromARGB(255, 12, 1, 73),
+                                      width: 100,
+                                      height: 60,
+                                      child: Center(
+                                          child: Image(
+                                              image: AssetImage(
+                                                  'assets/223.png'))),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.fromLTRB(30, 20, 0, 0),
+                        child: ClipRRect(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Container(
+                                color: Color.fromARGB(255, 12, 1, 73),
+                                width: 300,
+                                height: 70,
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      'Ban bè 1',
+                                      style: TextStyle(
+                                        color:
+                                            Color.fromARGB(255, 255, 255, 255),
+                                        fontWeight: FontWeight.normal,
+                                      ),
+                                    ),
+                                    Container(
+                                      color: Color.fromARGB(255, 12, 1, 73),
+                                      width: 100,
+                                      height: 60,
+                                      child: Center(
+                                          child: Image(
+                                              image: AssetImage(
+                                                  'assets/2234.png'))),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.fromLTRB(170, 10, 0, 0),
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: Text(
+                            'Thêm bạn bè +',
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 255, 255, 255),
+                              fontWeight: FontWeight.normal,
+                            ),
+                          ),
+                        ),
+                      )
+                    ]),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
