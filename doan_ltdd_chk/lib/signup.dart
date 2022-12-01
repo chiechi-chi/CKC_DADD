19989f3e14ef90addfcc9b81bdf5681222300d36
anhhoai <lephonganhhoai9a1@gmail.com>
2022-12-01 20:25:05 +0700

    hoaicommit

--- doan_ltdd_chk/lib/signup.dart
+++ doan_ltdd_chk/lib/signup.dart
@@ -1,110 +1,100 @@
 import 'package:flutter/material.dart';
 
-class login extends StatefulWidget {
-  const login({Key? key}) : super(key: key);
+class sigup extends StatefulWidget {
+  const sigup({
+    Key? key,
+  }) : super(key: key);
 
   @override
-  State<login> createState() => _loginState();
+  State<StatefulWidget> createState() {
+    return _sigup();
+  }
 }
 
-class _loginState extends State<login> {
+class _sigup extends State<sigup> {
   @override
   Widget build(BuildContext context) {
-    TextEditingController _emailController = TextEditingController();
-    TextEditingController _passwordController = TextEditingController();
-    return Padding(
-      padding: const EdgeInsets.all(0.00),
-      child: Container(
-        padding: const EdgeInsets.fromLTRB(20, 10, 20, 0),
-        decoration: BoxDecoration(
-          color: Color(0xff152B42),
+    return Scaffold(
+        body: Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      decoration: BoxDecoration(
+        color: Color(0xff152B42),
+      ),
+      padding: EdgeInsets.fromLTRB(30, 0, 0, 0),
+      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        const Text(
+          'Đăng kí tài khoản',
+          style: TextStyle(color: Colors.white, fontSize: 20),
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
+        Row(
           children: [
-            Container(
-              child: Column(children: [
-                Image.asset(
-                  'assets/h1.jpg',
-                ),
-                Container(
-                  width: MediaQuery.of(context).size.width * 0.9,
-                  child: TextField(
-                    controller: _emailController,
-                    style: TextStyle(color: Color.fromARGB(255, 14, 13, 13)),
-                    decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Color.fromARGB(255, 247, 245, 245),
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(30.0),
-                            borderSide: BorderSide(color: Colors.white)),
-                        hintText: 'Tài khoản',
-                        hintStyle: TextStyle(color: Colors.black)),
-                  ),
-                ),
-                const SizedBox(height: 10.0),
-                Container(
-                  width: MediaQuery.of(context).size.width * 0.9,
-                  child: TextField(
-                    obscureText: true,
-                    controller: _passwordController,
-                    style: TextStyle(color: Color.fromARGB(255, 14, 13, 13)),
-                    decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Colors.white,
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(30.0),
-                            borderSide: BorderSide(color: Colors.white)),
-                        hintText: 'Mật khẩu',
-                        hintStyle: TextStyle(color: Colors.black)),
-                  ),
-                ),
-              ]),
-            ),
-            const SizedBox(
-              height: 10.0,
+            const Text(
+              'Email:',
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.normal),
             ),
             Container(
-              child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      child: TextButton(
-                          style: ButtonStyle(
-                              foregroundColor: MaterialStateProperty.all<Color>(
-                                  Colors.white)),
-                          onPressed: () {},
-                          child: const Text('Quên mật khẩu')),
-                    ),
-                    const SizedBox(
-                      height: 10.0,
-                    ),
-                    Container(
-                      child: TextButton(
-                          style: ButtonStyle(
-                              foregroundColor: MaterialStateProperty.all<Color>(
-                                  Colors.white)),
-                          onPressed: () {},
-                          child: const Text('Đăng kí tài khoản')),
-                    ),
-                  ]),
-            ),
-            const SizedBox(
-              height: 10.0,
-            ),
-            Container(
-              width: double.infinity,
-              child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      primary: Color.fromARGB(255, 16, 66, 106),
-                      onPrimary: Colors.white),
-                  onPressed: () async {},
-                  child: const Text('Đăng nhập')),
+              padding: EdgeInsets.fromLTRB(55, 20, 0, 0),
+              width: MediaQuery.of(context).size.width * 0.75,
+              child: TextField(
+                style: TextStyle(color: Color.fromARGB(255, 20, 20, 20)),
+                decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                        borderSide: BorderSide(color: Colors.white)),
+                    hintText: 'Email',
+                    hintStyle: TextStyle(color: Colors.black)),
+              ),
             ),
           ],
         ),
-      ),
-    );
+        Padding(padding: EdgeInsets.all(10)),
+        Row(
+          children: [
+            const Text(
+              'Mật khẩu:',
+              style: TextStyle(
+                  color: Color.fromARGB(255, 247, 244, 244),
+                  fontWeight: FontWeight.normal),
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(30, 20, 0, 0),
+              width: MediaQuery.of(context).size.width * 0.7,
+              child: TextField(
+                obscureText: true,
+                style: TextStyle(color: Color.fromARGB(255, 19, 18, 18)),
+                decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                        borderSide: BorderSide(color: Colors.white)),
+                    hintText: 'Mật khẩu',
+                    hintStyle:
+                        TextStyle(color: Color.fromARGB(255, 19, 19, 19))),
+              ),
+            ),
+          ],
+        ),
+        Padding(padding: EdgeInsets.all(10)),
+        Container(
+          padding: EdgeInsets.fromLTRB(0, 15, 0, 0),
+          child: ElevatedButton(
+              style: ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll<Color>(
+                      Color.fromARGB(255, 51, 10, 146).withOpacity(0.8)),
+                  shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0)))),
+              onPressed: () async {},
+              child: const Padding(
+                padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                child: Text('Đăng Kí'),
+              )),
+        ),
+      ]),
+    ));
   }
 }
