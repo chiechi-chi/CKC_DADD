e2fff9e84d6cfe3bcded8e59a5b4918344cb8525
ncKhang <nckhangword@gmail.com>
2022-12-02 07:44:30 +0700

    choi 2 nguoi

---
+++ doan_ltdd_chk/lib/choihainguoi.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Choihainguoi extends StatefulWidget {
+  const Choihainguoi({super.key});
+
+  @override
+  State<Choihainguoi> createState() => _ChoihainguoiState();
+}
+
+class _ChoihainguoiState extends State<Choihainguoi> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
